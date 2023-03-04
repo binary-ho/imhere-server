57077d43a9a4a6ab3a3931de4aebf46aa2bbd617
Jinho <dfghcvb11@naver.com>
2023-03-05 03:32:54 +0900

    i'm here!

---
+++ src/main/java/gdsc/binaryho/imhere/ImhereApplication.java
@@ -0,0 +1,13 @@
+package gdsc.binaryho.imhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImhereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gdsc/binaryho/imhere/ImhereApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.binaryho.imhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImhereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
