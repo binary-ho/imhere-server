1ba583df516be100e8fed2c918af91cfc6b9f2e5
Jinho <dfghcvb11@naver.com>
2023-03-05 05:35:11 +0900

    feat : RolesConverter 구현

---
+++ src/main/java/gdsc/binaryho/imhere/domain/Infrastructure/RolesConverter.java
@@ -0,0 +1,31 @@
+package gdsc.binaryho.imhere.domain.Infrastructure;
+
+import gdsc.binaryho.imhere.domain.member.Role;
+import gdsc.binaryho.imhere.domain.member.Roles;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.persistence.AttributeConverter;
+
+public class RolesConverter implements AttributeConverter<Roles, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Roles roles) {
+        if (roles == null) {
+            return null;
+        }
+
+        String roleString = roles.getRoles().toString();
+        return roleString.substring(1, roleString.length() - 1);
+    }
+
+    @Override
+    public Roles convertToEntityAttribute(String rolesString) {
+        List<Role> roleList = Arrays.stream(rolesString.split(", "))
+            .map(Role::valueOf)
+            .collect(Collectors.toList());
+        return new Roles(EnumSet.copyOf(roleList));
+    }
+}
---
+++ src/test/java/gdsc/binaryho/imhere/domain/Infrastructure/RolesConverterTest.java
@@ -0,0 +1,33 @@
+package gdsc.binaryho.imhere.domain.Infrastructure;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import gdsc.binaryho.imhere.domain.member.Role;
+import gdsc.binaryho.imhere.domain.member.Roles;
+import java.util.EnumSet;
+import org.junit.jupiter.api.Test;
+
+class RolesConverterTest {
+
+    RolesConverter rolesConverter;
+
+    @Test
+    void Roles를_String으로_변환할_수_있다() {
+        Roles roles = new Roles(EnumSet.of(Role.GDSC_MEMBER, Role.OC_MEMBER, Role.LECTURER, Role.STUDENT));
+
+        rolesConverter = new RolesConverter();
+        String actualResult = rolesConverter.convertToDatabaseColumn(roles);
+        assertThat(actualResult).isEqualTo("GDSC_MEMBER, OC_MEMBER, LECTURER, STUDENT");
+    }
+
+    @Test
+    void String을_Roles로_변환할_수_있다() {
+        rolesConverter = new RolesConverter();
+        Roles expectedResult = new Roles(EnumSet.of(Role.GDSC_MEMBER, Role.OC_MEMBER, Role.LECTURER, Role.STUDENT));
+
+        Roles actualResult = rolesConverter.convertToEntityAttribute("GDSC_MEMBER, OC_MEMBER, LECTURER, STUDENT");
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
+}
