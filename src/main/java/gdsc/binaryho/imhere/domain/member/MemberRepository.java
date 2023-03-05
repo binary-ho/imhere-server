69dd5092a1f35a466cc364a98808e3ba90f5b5b5
Jinho <dfghcvb11@naver.com>
2023-03-06 00:33:48 +0900

    feat : CorsConfig 설정 구현

---
+++ src/main/java/gdsc/binaryho/imhere/domain/member/MemberRepository.java
@@ -0,0 +1,9 @@
+package gdsc.binaryho.imhere.domain.member;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findById(Long id);
+}
