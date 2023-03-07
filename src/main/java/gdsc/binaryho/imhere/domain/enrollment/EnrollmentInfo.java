6e4b062057d968d98951334124cfefc5bf37b45c
Jinho <dfghcvb11@naver.com>
2023-03-07 10:21:05 +0900

    refactor : 엔티티 테이블명 지정

--- src/main/java/gdsc/binaryho/imhere/domain/enrollment/EnrollmentInfo.java
+++ src/main/java/gdsc/binaryho/imhere/domain/enrollment/EnrollmentInfo.java
@@ -9,14 +9,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
+@Table(name = "enrollment_infos")
+@Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class EnrollmentInfo {
 
--- src/main/java/gdsc/binaryho/imhere/domain/member/Member.java
+++ src/main/java/gdsc/binaryho/imhere/domain/member/Member.java
@@ -11,14 +11,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
+@Table(name = "members")
+@Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
