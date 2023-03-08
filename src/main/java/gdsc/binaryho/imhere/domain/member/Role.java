c25291113132584b1756cfa783643afba73b179f
Jinho <dfghcvb11@naver.com>
2023-03-08 23:28:57 +0900

    refactor : ROLE GDSC_MEMBER, OC_MEMBER 제거, ADMIN 추가

--- src/main/java/gdsc/binaryho/imhere/domain/member/Role.java
+++ src/main/java/gdsc/binaryho/imhere/domain/member/Role.java
@@ -1,5 +1,5 @@
 package gdsc.binaryho.imhere.domain.member;
 
 public enum Role {
-    GDSC_MEMBER, OC_MEMBER, LECTURER, STUDENT
+    ADMIN, LECTURER, STUDENT
 }
--- src/test/java/gdsc/binaryho/imhere/domain/Infrastructure/RolesConverterTest.java
+++ src/test/java/gdsc/binaryho/imhere/domain/Infrastructure/RolesConverterTest.java
@@ -13,19 +13,19 @@ class RolesConverterTest {
 
     @Test
     void Roles를_String으로_변환할_수_있다() {
-        Roles roles = new Roles(EnumSet.of(Role.GDSC_MEMBER, Role.OC_MEMBER, Role.LECTURER, Role.STUDENT));
+        Roles roles = new Roles(EnumSet.of(Role.ADMIN, Role.LECTURER, Role.STUDENT));
 
         rolesConverter = new RolesConverter();
         String actualResult = rolesConverter.convertToDatabaseColumn(roles);
-        assertThat(actualResult).isEqualTo("GDSC_MEMBER, OC_MEMBER, LECTURER, STUDENT");
+        assertThat(actualResult).isEqualTo("ADMIN, LECTURER, STUDENT");
     }
 
     @Test
     void String을_Roles로_변환할_수_있다() {
         rolesConverter = new RolesConverter();
-        Roles expectedResult = new Roles(EnumSet.of(Role.GDSC_MEMBER, Role.OC_MEMBER, Role.LECTURER, Role.STUDENT));
+        Roles expectedResult = new Roles(EnumSet.of(Role.ADMIN, Role.LECTURER, Role.STUDENT));
 
-        Roles actualResult = rolesConverter.convertToEntityAttribute("GDSC_MEMBER, OC_MEMBER, LECTURER, STUDENT");
+        Roles actualResult = rolesConverter.convertToEntityAttribute("ADMIN, LECTURER, STUDENT");
         assertThat(actualResult).isEqualTo(expectedResult);
     }
 }
