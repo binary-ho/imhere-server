54823fd477eb798f1ad46d73604ba8f25dd1fa97
Jinho <dfghcvb11@naver.com>
2023-03-10 01:49:14 +0900

    feat : Authentication을 위한 PrincipalDetails Service 구현

---
+++ src/main/java/gdsc/binaryho/imhere/config/auth/PrincipalDetails.java
@@ -0,0 +1,53 @@
+package gdsc.binaryho.imhere.config.auth;
+
+import gdsc.binaryho.imhere.domain.member.Member;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class PrincipalDetails implements UserDetails {
+
+    private final Member member;
+
+    public PrincipalDetails(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add((GrantedAuthority) () -> member.getRole().getKey());
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
---
+++ src/main/java/gdsc/binaryho/imhere/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,23 @@
+package gdsc.binaryho.imhere.config.auth;
+
+import gdsc.binaryho.imhere.domain.member.Member;
+import gdsc.binaryho.imhere.domain.member.MemberRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    public PrincipalDetailsService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String univId) {
+        Member member = memberRepository.findByUnivId(univId).orElseThrow();
+        return new PrincipalDetails(member);
+    }
+}
