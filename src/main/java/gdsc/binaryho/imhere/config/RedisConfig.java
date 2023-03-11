4f60ef2003dace88cff79c1b9816c5eb4ea02d11
Jinho <dfghcvb11@naver.com>
2023-03-11 17:59:13 +0900

    feat : Redis 도입 - 기본 설정

---
+++ src/main/java/gdsc/binaryho/imhere/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package gdsc.binaryho.imhere.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        configuration.setPassword(redisPassword);
+
+        return new LettuceConnectionFactory(configuration);
+    }
+}
