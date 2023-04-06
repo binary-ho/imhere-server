370a05a40d98389bf79077da310264d88c075175
binary_ho <dfghcvb11@naver.com>
2023-04-06 23:59:31 +0900

    refactor : 메인 도메인 외에도 test 서버 도메인도 Cors허용 처리

--- src/main/java/gdsc/binaryho/imhere/config/CorsConfig.java
+++ src/main/java/gdsc/binaryho/imhere/config/CorsConfig.java
@@ -1,6 +1,8 @@
 package gdsc.binaryho.imhere.config;
 
 import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,6 +11,11 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
+
+    @Value("${origin.domain}")
+    private String productionOrigin;
+    @Value("${origin.test}")
+    private String testOrigin;
 
     @Bean
     public CorsFilter corsFilter() {
@@ -21,7 +28,7 @@ public class CorsConfig {
     private CorsConfiguration getCorsConfiguration() {
         CorsConfiguration configuration =  new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.addAllowedOrigin("https://imhere.im");
+        configuration.setAllowedOrigins(List.of(productionOrigin, testOrigin));
         configuration.addAllowedHeader("*");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS"));
 
