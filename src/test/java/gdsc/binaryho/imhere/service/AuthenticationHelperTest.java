0d627b95fde23886a90982c1353c34eba8e5d57f
binary_ho <dfghcvb11@naver.com>
2023-04-16 01:41:35 +0900

    test : AuthenticationHelper public 메서드 전체 테스트 작성 (#8)

--- src/test/java/gdsc/binaryho/imhere/service/AuthenticationHelperTest.java
+++ src/test/java/gdsc/binaryho/imhere/service/AuthenticationHelperTest.java
@@ -5,10 +5,12 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import gdsc.binaryho.imhere.MockMember;
 import gdsc.binaryho.imhere.domain.member.Member;
+import gdsc.binaryho.imhere.domain.member.Role;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,6 +27,27 @@ class AuthenticationHelperTest {
             Member actualLoginMember = authenticationHelper.getCurrentMember();
             assertThat(actualLoginMember).isNotNull();
         } catch (ParameterResolutionException e) {
+            fail();
+        }
+    }
+
+    @Test
+    @MockMember
+    void 현재_로그인된_유저가_입력된_아이디를_가졌는지_검증할_수_있다() {
+        try {
+            Member loginMember = authenticationHelper.getCurrentMember();
+            authenticationHelper.verifyRequestMemberLogInMember(loginMember.getId());
+        } catch (AccessDeniedException e) {
+            fail();
+        }
+    }
+
+    @Test
+    @MockMember(role = Role.ADMIN)
+    void 현재_로그인된_유저가_Admin인지_확인할_수_있다() {
+        try {
+            authenticationHelper.verifyMemberHasAdminRole();
+        } catch (AccessDeniedException e) {
             fail();
         }
     }
