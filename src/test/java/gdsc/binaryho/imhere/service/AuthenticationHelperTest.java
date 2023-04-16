244aec34bf7ab606c6c12b5c380d85c3e995c27b
binary_ho <dfghcvb11@naver.com>
2023-04-17 00:24:11 +0900

    chore : test 환경 분이리로 어노테이션 제거 (#8)

--- src/test/java/gdsc/binaryho/imhere/service/AuthenticationHelperTest.java
+++ src/test/java/gdsc/binaryho/imhere/service/AuthenticationHelperTest.java
@@ -11,10 +11,8 @@ import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
+@SpringBootTest
 class AuthenticationHelperTest {
 
     @Autowired
