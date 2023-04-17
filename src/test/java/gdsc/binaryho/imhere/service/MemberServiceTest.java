3126b1b18ccdb0cfa067ea204dc162fdc6e1a779
binary_ho <dfghcvb11@naver.com>
2023-04-17 14:13:56 +0900

    test : Member Service validateSignInRequest 관련 테스트 코드 작성 (#8)

--- src/test/java/gdsc/binaryho/imhere/service/MemberServiceTest.java
+++ src/test/java/gdsc/binaryho/imhere/service/MemberServiceTest.java
@@ -7,6 +7,9 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import gdsc.binaryho.imhere.domain.member.Member;
 import gdsc.binaryho.imhere.domain.member.MemberRepository;
+import gdsc.binaryho.imhere.mapper.dtos.SignInRequestValidationResult;
+import gdsc.binaryho.imhere.mapper.requests.SignInRequest;
+import java.security.InvalidParameterException;
 import javax.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,6 +17,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
@@ -24,17 +28,24 @@ class MemberServiceTest {
     private MemberRepository memberRepository;
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private AuthenticationHelper authenticationHelper;
 
     String UNIV_ID = "UNIV_ID";
     String NAME = "이진호";
     String PASSWORD = "abcd1234";
+    String DEFAULT_MEMBER_ROLE = "ROLE_STUDENT";
 
     @Test
     @Transactional
     void 회원가입을_할_수_있다() {
+
         try {
             memberService.signUp(UNIV_ID, NAME, PASSWORD);
-            Member member = memberRepository.findByUnivId(UNIV_ID).orElseThrow();
+
+            Member member = memberRepository.findByUnivId(UNIV_ID)
+                .orElseThrow();
+
             assertAll(
                 () -> assertThat(UNIV_ID).isEqualTo(UNIV_ID),
                 () -> assertThat(member.getUnivId()).isEqualTo(UNIV_ID),
@@ -49,7 +60,9 @@ class MemberServiceTest {
     @Test
     @Transactional
     void 중복된_UnivId로_회원가입_시도시_중복_예외를_던진다() {
+
         memberService.signUp(UNIV_ID, NAME, PASSWORD);
+
         assertThatThrownBy(() -> memberService.signUp(UNIV_ID, NAME + "2", PASSWORD + "2"))
             .isInstanceOf(DuplicateKeyException.class);
     }
@@ -58,7 +71,39 @@ class MemberServiceTest {
     @ValueSource(strings = {"abcdabcd", "12341234", "dafadfdafdafdfadfadfadfadafdaf", "a1"})
     @Transactional
     void 회원가입시_비밀번호_형식이_잘못되면_예외를_던진다(String password) {
+
         assertThatThrownBy(() -> memberService.signUp(UNIV_ID, NAME, password))
             .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로그인_요청_검증시_가입하지_않은_회원인_경우_예외를_던진다() {
+
+        assertThatThrownBy(
+            () -> memberService.validateSignInRequest(new SignInRequest(UNIV_ID, PASSWORD))
+        ).isInstanceOf(AuthenticationException.class);
+    }
+
+    @Test
+    @Transactional
+    void 로그인_요청_검증시_비밀번호가_틀린_경우_예외를_던진다() {
+
+        memberService.signUp(UNIV_ID, NAME, PASSWORD);
+
+        assertThatThrownBy(
+            () -> memberService.validateSignInRequest(new SignInRequest(UNIV_ID, PASSWORD + PASSWORD))
+        ).isInstanceOf(InvalidParameterException.class);
+    }
+
+    @Test
+    @Transactional
+    void 로그인_요청을_검증할_수_있다() {
+
+        memberService.signUp(UNIV_ID, NAME, PASSWORD);
+
+        SignInRequestValidationResult signInRequestValidationResult =
+            memberService.validateSignInRequest(new SignInRequest(UNIV_ID, PASSWORD));
+
+        assertThat(signInRequestValidationResult.getRoleKey()).isEqualTo(DEFAULT_MEMBER_ROLE);
     }
 }
