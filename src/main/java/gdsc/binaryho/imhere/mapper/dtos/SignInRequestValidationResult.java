12af47ae9c125774b89b0d61004632af4f09aa1d
binary_ho <dfghcvb11@naver.com>
2023-04-17 13:46:32 +0900

    refactor : Member Service login 메서드의 이름을 책임에 맞게 validateSignInRequest로 변경하고, 그에 따라 SignInResponseDto에서 univId 필드를 제거한 다음 SignInRequestValidationResult로 이름 변경 (#8)

---
+++ src/main/java/gdsc/binaryho/imhere/mapper/dtos/SignInRequestValidationResult.java
@@ -0,0 +1,15 @@
+package gdsc.binaryho.imhere.mapper.dtos;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Getter;
+
+@Getter
+@Tag(name = "SignInRequestValidationResult", description = "로그인한 유저 id와 비밀번호를 검증한 다음 유저의 권한을 반환한다")
+public class SignInRequestValidationResult {
+
+    private final String roleKey;
+
+    public SignInRequestValidationResult(String roleKey) {
+        this.roleKey = roleKey;
+    }
+}
