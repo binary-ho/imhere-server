3811bbedf9801c6bbfab1accbaf7e347802bc0cc
binary_ho <dfghcvb11@naver.com>
2023-05-09 11:20:27 +0900

    refactor : error code 100번대에서 1000번대로 변경 (#22)

--- src/main/java/gdsc/binaryho/imhere/exception/ErrorCode.java
+++ src/main/java/gdsc/binaryho/imhere/exception/ErrorCode.java
@@ -8,25 +8,25 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, 440, "로그인 회원 정보가 없습니다."),
-    PASSWORD_INCORRECT(HttpStatus.UNAUTHORIZED, 441, "로그인 회원 비밀번호가 불일치합니다."),
-    EMAIL_DUPLICATED(HttpStatus.CONFLICT, 443, "이미 가입된 Email 입니다."),
-    EMAIL_FORMAT_MISMATCH(HttpStatus.BAD_REQUEST, 445, "형식에 맞는 이메일을 입력하세요."),
-    PASSWORD_FORMAT_MISMATCH(HttpStatus.BAD_REQUEST, 446, "형식에 맞는 비밀번호를 입력하세요."),
-    REQUEST_FORMAT_MISMATCH(HttpStatus.BAD_REQUEST, 447, "요청 형식이 맞지 않습니다."),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, 1001, "로그인 회원 정보가 없습니다."),
+    PASSWORD_INCORRECT(HttpStatus.UNAUTHORIZED, 1002, "로그인 회원 비밀번호가 불일치합니다."),
+    EMAIL_DUPLICATED(HttpStatus.CONFLICT, 1003, "이미 가입된 Email 입니다."),
+    EMAIL_FORMAT_MISMATCH(HttpStatus.BAD_REQUEST, 1004, "형식에 맞는 이메일을 입력하세요."),
+    PASSWORD_FORMAT_MISMATCH(HttpStatus.BAD_REQUEST, 1005, "형식에 맞는 비밀번호를 입력하세요."),
+    REQUEST_FORMAT_MISMATCH(HttpStatus.BAD_REQUEST, 1006, "요청 형식이 맞지 않습니다."),
 
-    LECTURE_NOT_FOUND(HttpStatus.NOT_FOUND, 450, "강의 정보가 없습니다."),
-    LECTURE_NOT_OPEN(HttpStatus.FORBIDDEN, 451, "강의에 출석 가능한 상태가 아닙니다. (Lecture Not OPEN)"),
+    LECTURE_NOT_FOUND(HttpStatus.NOT_FOUND, 2001, "강의 정보가 없습니다."),
+    LECTURE_NOT_OPEN(HttpStatus.FORBIDDEN, 2002, "강의에 출석 가능한 상태가 아닙니다. (Lecture Not OPEN)"),
 
-    ENROLLMENT_NOT_FOUND(HttpStatus.NOT_FOUND, 461, "수강신청 정보 없음"),
-    ENROLLMENT_DUPLICATED(HttpStatus.CONFLICT, 462, "수강신청 중복 요청"),
-    ENROLLMENT_NOT_APPROVED(HttpStatus.FORBIDDEN, 463, "수강신청 승인되지 않음"),
+    ENROLLMENT_NOT_FOUND(HttpStatus.NOT_FOUND, 3001, "수강신청 정보 없음"),
+    ENROLLMENT_DUPLICATED(HttpStatus.CONFLICT, 3002, "수강신청 중복 요청"),
+    ENROLLMENT_NOT_APPROVED(HttpStatus.FORBIDDEN, 3003, "수강신청 승인되지 않음"),
 
-    ATTENDANCE_NUMBER_INCORRECT(HttpStatus.BAD_REQUEST, 470, "출석 번호 불일치"),
-    ATTENDANCE_TIME_EXCEEDED(HttpStatus.BAD_REQUEST, 471, "출석 가능 시간 초과"),
+    ATTENDANCE_NUMBER_INCORRECT(HttpStatus.BAD_REQUEST, 4001, "출석 번호 불일치"),
+    ATTENDANCE_TIME_EXCEEDED(HttpStatus.BAD_REQUEST, 4002, "출석 가능 시간 초과"),
 
-    REQUEST_MEMBER_ID_MISMATCH(HttpStatus.BAD_REQUEST, 490, "실제로 요청을 보낸 유저의 id와 Request에 기재된 id가 다릅니다. (악의적 요청)"),
-    PERMISSION_DENIED(HttpStatus.FORBIDDEN, 491, "유저가 권한이 없는 요청을 보냈습니다. (악의적 요청)"),
+    REQUEST_MEMBER_ID_MISMATCH(HttpStatus.BAD_REQUEST, 9001, "실제로 요청을 보낸 유저의 id와 Request에 기재된 id가 다릅니다. (악의적 요청)"),
+    PERMISSION_DENIED(HttpStatus.FORBIDDEN, 9002, "유저가 권한이 없는 요청을 보냈습니다. (악의적 요청)"),
 
     ;
 
