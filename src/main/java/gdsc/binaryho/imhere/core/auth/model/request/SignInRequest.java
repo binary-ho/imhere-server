ac2db89ee756e06c4504a3f64478724e15985a10
binary_ho <dfghcvb11@naver.com>
2023-07-23 13:55:44 +0900

    refactor : SignInRequest 에서 필요없는 NoArgsConstructor 제거 (#38)

--- src/main/java/gdsc/binaryho/imhere/core/auth/model/request/SignInRequest.java
+++ src/main/java/gdsc/binaryho/imhere/core/auth/model/request/SignInRequest.java
@@ -1,13 +1,10 @@
 package gdsc.binaryho.imhere.core.auth.model.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class SignInRequest {
 
