0647b9560d79848a4ae69effcf4233e3d4f28df3
binary_ho <dfghcvb11@naver.com>
2023-07-23 15:47:22 +0900

    refactor : EmailSender의 예외를 모두 처리했으므로, Controller에서 Exception Catch 제거 (#40)

--- src/main/java/gdsc/binaryho/imhere/core/auth/controller/AuthController.java
+++ src/main/java/gdsc/binaryho/imhere/core/auth/controller/AuthController.java
@@ -6,13 +6,9 @@ import gdsc.binaryho.imhere.core.auth.util.EmailSender;
 import gdsc.binaryho.imhere.exception.ImhereException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.io.UnsupportedEncodingException;
-import javax.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.MailException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,15 +48,8 @@ public class AuthController {
     @Operation(summary = "특정 이메일로 회원가입 코드를 발급하여 발송하는 API")
     @PostMapping("/verification/{email}")
     public ResponseEntity<Void> generateVerificationNumber(@PathVariable("email") String email) {
-        try {
-            emailSender.sendMailAndGetVerificationCode(email);
-            return ResponseEntity.ok().build();
-        } catch (MailException | MessagingException | UnsupportedEncodingException error) {
-            log.info("[이메일 인증 번호 전송 에러] email : {}", email);
-            return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .build();
-        }
+        emailSender.sendMailAndGetVerificationCode(email);
+        return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "특정 이메일에 발급된 회원가입 코드와 입력된 코드의 일치여부를 확인하는 API")
