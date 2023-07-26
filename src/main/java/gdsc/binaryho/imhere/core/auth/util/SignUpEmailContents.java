ba1f6eb1818d8bd0c56b6f2f3b8b10a417ec74a6
binary_ho <dfghcvb11@naver.com>
2023-07-27 02:56:23 +0900

    chore : 메일에 적히는 constant String들을 상수 클래스에 옮김 (#45)

---
+++ src/main/java/gdsc/binaryho/imhere/core/auth/util/SignUpEmailContents.java
@@ -0,0 +1,19 @@
+package gdsc.binaryho.imhere.core.auth.util;
+
+public class SignUpEmailContents {
+
+    public static final String SENDER_PERSONAL = "jinholee";
+    public static final String SENDER_ADDRESS = "gdscimhere@gmail.com";
+    public static final String MESSAGE_SUBJECT = "Hello There! GDSC Hongik i'm here 회원가입 인증 코드입니다.";
+    public static final String MESSAGE_PREFIX = ""
+        + "<div style='margin:20px;'>"
+        + "<div style='margin:20px;'>"
+        + "<h1> GDSC Hongik i'm here 인증 코드 입니다! </h1> <br>"
+        + "CODE : <strong>";
+
+    public static final String MESSAGE_SUFFIX = ""
+        + "</strong>"
+        + "<h3> 10분안에 입력 부탁드립니다. 감사합니다! <h3>"
+        + "<br>";
+}
+
