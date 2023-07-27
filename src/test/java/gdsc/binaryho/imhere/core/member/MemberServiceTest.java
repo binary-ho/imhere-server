cee4c4883f54a31c70ff67789120eb7aea3a8824
binary_ho <dfghcvb11@naver.com>
2023-07-28 02:52:11 +0900

    test : MemberService 테스트 코드 보충 (#35)

--- src/test/java/gdsc/binaryho/imhere/core/member/MemberServiceTest.java
+++ src/test/java/gdsc/binaryho/imhere/core/member/MemberServiceTest.java
@@ -1,18 +1,22 @@
 package gdsc.binaryho.imhere.core.member;
 
 import static gdsc.binaryho.imhere.fixture.MemberFixture.UNIV_ID;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import gdsc.binaryho.imhere.MockSecurityContextMember;
+import gdsc.binaryho.imhere.core.auth.exception.MemberNotFoundException;
+import gdsc.binaryho.imhere.core.auth.exception.PermissionDeniedException;
 import gdsc.binaryho.imhere.core.auth.util.AuthenticationHelper;
 import gdsc.binaryho.imhere.core.member.application.MemberService;
 import gdsc.binaryho.imhere.core.member.infrastructure.MemberRepository;
 import gdsc.binaryho.imhere.core.member.model.request.RoleChangeRequest;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
@@ -46,5 +50,29 @@ class MemberServiceTest {
         verify(mockStudent, times(1)).setRole(Role.valueOf(roleKey));
     }
 
-    // TODO : 예외 상황 테스트 작성
+    @Test
+    @MockSecurityContextMember(role = Role.STUDENT)
+    void Admin이_아닌_경우_다른_회원의_권한_변경을_시도하면_예외가_발생한다() {
+        // when
+        RoleChangeRequest roleChangeRequest = new RoleChangeRequest(Role.STUDENT.getKey());
+
+        // then
+        assertThatThrownBy(
+            () ->  memberService.memberRoleChange(roleChangeRequest, UNIV_ID)
+        ).isInstanceOf(PermissionDeniedException.class);
+    }
+
+    @Test
+    @MockSecurityContextMember(role = Role.ADMIN)
+    void 없는_회원의_권한_변경을_시도하면_예외가_발생한다() {
+        given(memberRepository.findByUnivId(UNIV_ID)).willReturn(Optional.empty());
+
+        // when
+        RoleChangeRequest roleChangeRequest = new RoleChangeRequest(Role.STUDENT.getKey());
+
+        // then
+        assertThatThrownBy(
+            () ->  memberService.memberRoleChange(roleChangeRequest, UNIV_ID + 777L)
+        ).isInstanceOf(MemberNotFoundException.class);
+    }
 }
