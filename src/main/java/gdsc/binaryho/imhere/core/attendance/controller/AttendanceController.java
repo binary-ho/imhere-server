2564b5db0b2ed47caa39dd1844a9bc97e98c5cae
binary_ho <dfghcvb11@naver.com>
2023-08-10 22:05:46 +0900

    chore : API 버저닝 삭제 반영 (#55)

--- src/main/java/gdsc/binaryho/imhere/core/attendance/controller/AttendanceController.java
+++ src/main/java/gdsc/binaryho/imhere/core/attendance/controller/AttendanceController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @Log4j2
 @Tag(name = "Attendance", description = "출석 기능 관련 API입니다.")
 @RestController
