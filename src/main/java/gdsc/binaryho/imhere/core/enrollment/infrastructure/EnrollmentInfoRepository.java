641a186c110b8b7e1ac6c8cf7e10cca8ccf9bb2b
binary_ho <dfghcvb11@naver.com>
2023-08-14 23:27:01 +0900

    refactor : 학생이 자신의 수강 신청이 승인되었고, 열려있는 강의를 바로 가져오는 메서드를 작성하고 기존의 EnrollmentInfoRepository의 메서드 삭제  (#57)

--- src/main/java/gdsc/binaryho/imhere/core/enrollment/infrastructure/EnrollmentInfoRepository.java
+++ src/main/java/gdsc/binaryho/imhere/core/enrollment/infrastructure/EnrollmentInfoRepository.java
@@ -3,7 +3,6 @@ package gdsc.binaryho.imhere.core.enrollment.infrastructure;
 import gdsc.binaryho.imhere.core.enrollment.EnrollmentInfo;
 import gdsc.binaryho.imhere.core.enrollment.EnrollmentState;
 import gdsc.binaryho.imhere.core.lecture.Lecture;
-import gdsc.binaryho.imhere.core.lecture.LectureState;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,7 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface EnrollmentInfoRepository extends JpaRepository<EnrollmentInfo, Long> {
 
     List<EnrollmentInfo> findAllByMemberIdAndEnrollmentState(Long memberId, EnrollmentState enrollmentState);
-    List<EnrollmentInfo> findAllByMemberIdAndLecture_LectureStateAndEnrollmentState(Long memberId, LectureState lectureState, EnrollmentState enrollmentState);
     List<EnrollmentInfo> findAllByLecture(Lecture lecture);
     List<EnrollmentInfo> findAllByLectureAndEnrollmentState(Lecture lecture, EnrollmentState enrollmentState);
     Optional<EnrollmentInfo> findByMemberIdAndLectureIdAndEnrollmentState(Long memberId, Long lectureId, EnrollmentState enrollmentState);
--- src/main/java/gdsc/binaryho/imhere/core/lecture/infrastructure/LectureRepository.java
+++ src/main/java/gdsc/binaryho/imhere/core/lecture/infrastructure/LectureRepository.java
@@ -2,13 +2,17 @@ package gdsc.binaryho.imhere.core.lecture.infrastructure;
 
 import gdsc.binaryho.imhere.core.lecture.Lecture;
 import gdsc.binaryho.imhere.core.lecture.LectureState;
+import io.lettuce.core.dynamic.annotation.Param;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
 
     Optional<Lecture> findById(Long id);
     List<Lecture> findAllByMemberId(Long id);
     List<Lecture> findAllByLectureStateNot(LectureState lectureState);
+    @Query("SELECT e.lecture FROM EnrollmentInfo e WHERE e.member.id = :memberId AND e.enrollmentState = 'APPROVAL' AND e.lecture.lectureState = 'OPEN'")
+    List<Lecture> findOpenAndApprovalLecturesByMemberId(@Param("memberId") Long memberId);
 }
