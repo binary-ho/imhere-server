0094efb8f5e11e3b271d49de1ce68ea562de75be
binary_ho <dfghcvb11@naver.com>
2023-08-17 18:47:20 +0900

    chore : Key Prefix가 특수문자로 시작하도록 변경 (#60)

--- src/main/java/gdsc/binaryho/imhere/config/redis/RedisKeyPrefixes.java
+++ src/main/java/gdsc/binaryho/imhere/config/redis/RedisKeyPrefixes.java
@@ -2,6 +2,6 @@ package gdsc.binaryho.imhere.config.redis;
 
 public class RedisKeyPrefixes {
 
-    public static final String ATTENDANCE_NUMBER_KEY_PREFIX = "lecture_id$";
-    public static final String VERIFICATION_CODE_KEY_PREFIX = "email$";
+    public static final String ATTENDANCE_NUMBER_KEY_PREFIX = "$lecture_id$";
+    public static final String VERIFICATION_CODE_KEY_PREFIX = "$email$";
 }
