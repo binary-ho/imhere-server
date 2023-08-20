88abf4a94625f21a670f0bb26d92a9edd7c5ac09
binary_ho <dfghcvb11@naver.com>
2023-08-21 00:01:35 +0900

    chore : OPEN_LECTURE_KEY_PREFIX 추가 (#62)

--- src/main/java/gdsc/binaryho/imhere/config/redis/RedisKeyPrefixes.java
+++ src/main/java/gdsc/binaryho/imhere/config/redis/RedisKeyPrefixes.java
@@ -4,4 +4,5 @@ public class RedisKeyPrefixes {
 
     public static final String ATTENDANCE_NUMBER_KEY_PREFIX = "$lecture_id$";
     public static final String VERIFICATION_CODE_KEY_PREFIX = "$email$";
+    public static final String OPEN_LECTURE_KEY_PREFIX = "$open_lecture$";
 }
