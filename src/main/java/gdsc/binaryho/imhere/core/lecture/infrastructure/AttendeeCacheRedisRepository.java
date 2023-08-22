35923dfffa155a0d050699ec8e039916480f2ff4
binary_ho <dfghcvb11@naver.com>
2023-08-23 06:24:17 +0900

    refactor : AttendeeCacheRedisRepository의 자료형 변경으로 인해 발생하는 예외 catch (#62)

--- src/main/java/gdsc/binaryho/imhere/core/lecture/infrastructure/AttendeeCacheRedisRepository.java
+++ src/main/java/gdsc/binaryho/imhere/core/lecture/infrastructure/AttendeeCacheRedisRepository.java
@@ -3,13 +3,16 @@ package gdsc.binaryho.imhere.core.lecture.infrastructure;
 import gdsc.binaryho.imhere.config.redis.RedisKeyPrefixes;
 import gdsc.binaryho.imhere.core.lecture.application.port.AttendeeCacheRepository;
 import gdsc.binaryho.imhere.core.lecture.model.StudentIds;
+import java.util.Collections;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.redis.connection.DataType;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
+@Log4j2
 @Repository
 @RequiredArgsConstructor
 public class AttendeeCacheRedisRepository implements AttendeeCacheRepository {
@@ -20,11 +23,17 @@ public class AttendeeCacheRedisRepository implements AttendeeCacheRepository {
 
     @Override
     public Set<Long> findAllAttendLectureId(Long studentId) {
-        String queryKey = KEY_PREFIX + studentId.toString();
+        String queryKey = KEY_PREFIX + studentId;
         DataType dataType = redisTemplate.type(queryKey);
 
         AttendeeFindStrategy readStrategy = AttendeeFindStrategy.fromDataType(dataType);
-        return readStrategy.findLectureIds(redisTemplate, queryKey);
+        try {
+            return readStrategy.findLectureIds(redisTemplate, queryKey);
+        } catch (RuntimeException exception) {
+            log.info("[캐시 READ 실패] findAllAttendLectureId 실패 studentID : {}, errorMessage : {}",
+                () -> studentId, () -> exception.getMessage());
+            return Collections.emptySet();
+        }
     }
 
     @Override
@@ -40,7 +49,12 @@ public class AttendeeCacheRedisRepository implements AttendeeCacheRepository {
         String key = KEY_PREFIX + studentId;
         DataType dataType = redisTemplate.type(key);
         AttendeeCacheStrategy saveStrategy = AttendeeCacheStrategy.fromDataType(dataType);
-        saveStrategy.cache(redisTemplate, key, lectureId);
-        redisTemplate.expire(key, LECTURE_STUDENT_EXPIRE_TIME, TimeUnit.MINUTES);
+        try {
+            saveStrategy.cache(redisTemplate, key, lectureId);
+            redisTemplate.expire(key, LECTURE_STUDENT_EXPIRE_TIME, TimeUnit.MINUTES);
+        } catch (RuntimeException exception) {
+            log.info("[캐시 WRITE 실패] cacheAttendee 실패 studentID : {}, lectureId : {} errorMessage : {}",
+                () -> studentId, () -> lectureId, () -> exception.getMessage());
+        }
     }
 }
