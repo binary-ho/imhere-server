ae2825dd95a6ce8c64a74db1231c40e2e8fda5f7
binary_ho <dfghcvb11@naver.com>
2023-08-22 15:19:40 +0900

    refactor : Lecture의 Approved Enrollment를 가져오는 메서드 쿼리 변경  (#62)

--- src/main/java/gdsc/binaryho/imhere/core/enrollment/infrastructure/EnrollmentInfoRepository.java
+++ src/main/java/gdsc/binaryho/imhere/core/enrollment/infrastructure/EnrollmentInfoRepository.java
@@ -6,12 +6,20 @@ import gdsc.binaryho.imhere.core.lecture.Lecture;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EnrollmentInfoRepository extends JpaRepository<EnrollmentInfo, Long> {
 
-    List<EnrollmentInfo> findAllByMemberIdAndEnrollmentState(Long memberId, EnrollmentState enrollmentState);
+    List<EnrollmentInfo> findAllByMemberIdAndEnrollmentState(Long memberId,
+        EnrollmentState enrollmentState);
+
     List<EnrollmentInfo> findAllByLecture(Lecture lecture);
-    List<EnrollmentInfo> findAllByLectureAndEnrollmentState(Lecture lecture, EnrollmentState enrollmentState);
-    Optional<EnrollmentInfo> findByMemberIdAndLectureIdAndEnrollmentState(Long memberId, Long lectureId, EnrollmentState enrollmentState);
+
+    @Query("SELECT e FROM EnrollmentInfo e WHERE e.lecture.id = :lectureId AND e.enrollmentState = 'APPROVAL'")
+    List<EnrollmentInfo> findAllApprovedByLectureId(Long lectureId);
+
+    Optional<EnrollmentInfo> findByMemberIdAndLectureIdAndEnrollmentState(Long memberId,
+        Long lectureId, EnrollmentState enrollmentState);
+
     Optional<EnrollmentInfo> findByMemberIdAndLectureId(Long memberId, Long lectureId);
 }
