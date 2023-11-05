13e69b0cce74d342c175bca7d6c4e4b9ab88951d
binary_ho <dfghcvb11@naver.com>
2023-11-06 00:11:51 +0900

    모니터링 툴 도입 #103 (#105)
    * feat : actuator prometheus 의존성 추가 (#103)
    
    * feat : actuator 데이터 수집을 위한 Basic Auth 인증 추가 (#103)
    
    * feat : Basic Auth UserDetails Value 추가 (#103)

--- src/main/java/gdsc/binaryho/imhere/security/config/SecurityConfig.java
+++ src/main/java/gdsc/binaryho/imhere/security/config/SecurityConfig.java
@@ -6,14 +6,20 @@ import gdsc.binaryho.imhere.security.filter.JwtAuthenticationFilter;
 import gdsc.binaryho.imhere.security.filter.JwtAuthorizationFilter;
 import gdsc.binaryho.imhere.security.jwt.TokenService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -32,6 +38,15 @@ public class SecurityConfig {
 
     private final TokenService tokenService;
 
+    @Value("${actuator.username}")
+    private String ACTUATOR_USERNAME;
+
+    @Value("${actuator.password}")
+    private String ACTUATOR_PASSWORD;
+
+    @Value("${actuator.role}")
+    private String ACTUATOR_ROLE;
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -44,6 +59,26 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Order(0)
+    public SecurityFilterChain filterChainForActuator(HttpSecurity http) throws Exception {
+        http
+            .requestMatchers()
+            .antMatchers("/system/actuator/**")
+
+            .and()
+            .httpBasic()
+
+            .and()
+            .userDetailsService(getActuatorUserDetailsService())
+
+            .cors().and()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
+    @Bean
+    @Order(1)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement()
@@ -56,7 +91,8 @@ public class SecurityConfig {
 
             .authorizeRequests()
 
-            .antMatchers("/login", "/logout", "/member/**", "/swagger*/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**")
+            .antMatchers("/login", "/logout", "/member/**",
+                "/swagger*/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**")
             .permitAll()
 
             .antMatchers("/api/admin/**")
@@ -67,9 +103,22 @@ public class SecurityConfig {
 
             .anyRequest().authenticated();
 
-        http.addFilterBefore(new JwtAuthenticationFilter(authenticationManager(authenticationConfiguration), tokenService), UsernamePasswordAuthenticationFilter.class);
-        http.addFilterBefore(new JwtAuthorizationFilter(authenticationManager(authenticationConfiguration), tokenService, memberRepository), BasicAuthenticationFilter.class);
+        http.addFilterBefore(new JwtAuthenticationFilter(
+            authenticationManager(authenticationConfiguration), tokenService), UsernamePasswordAuthenticationFilter.class);
+
+        http.addFilterBefore(new JwtAuthorizationFilter(
+            authenticationManager(authenticationConfiguration), tokenService, memberRepository), BasicAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    private UserDetailsService getActuatorUserDetailsService() {
+        String encodedPassword = passwordEncoder().encode(ACTUATOR_PASSWORD);
+        UserDetails userDetails = User.withUsername(ACTUATOR_USERNAME)
+            .password(encodedPassword)
+            .roles(ACTUATOR_ROLE)
+            .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
