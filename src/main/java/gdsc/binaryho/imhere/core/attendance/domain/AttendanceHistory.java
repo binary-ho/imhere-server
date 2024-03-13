53f09534cc6cf63928ee95625d10a2d2f27fefe1
binary_ho <dfghcvb11@naver.com>
2024-03-14 01:09:29 +0900

    feat : 출석 기록을 저장하는 AttendanceHistory 구현 (#117)

---
+++ src/main/java/gdsc/binaryho/imhere/core/attendance/domain/AttendanceHistory.java
@@ -0,0 +1,30 @@
+package gdsc.binaryho.imhere.core.attendance.domain;
+
+import gdsc.binaryho.imhere.config.redis.RedisKeyPrefixes;
+import gdsc.binaryho.imhere.domain.CacheEntity;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class AttendanceHistory extends CacheEntity {
+
+    private static final String KEY_FORMAT = RedisKeyPrefixes.ATTENDANCE_HISTORY_KEY_FORMAT;
+
+    private final long lectureId;
+    private final long studentId;
+    private final String timestamp;
+
+    public static AttendanceHistory of(long lectureId, long studentId, String timestamp) {
+        return new AttendanceHistory(lectureId, studentId, timestamp);
+    }
+
+    @Override
+    public String getKey() {
+        return String.format(KEY_FORMAT, lectureId, studentId);
+    }
+
+    public static String convertToKey(long lectureId, long studentId) {
+        return String.format(KEY_FORMAT, lectureId, studentId);
+    }
+}
