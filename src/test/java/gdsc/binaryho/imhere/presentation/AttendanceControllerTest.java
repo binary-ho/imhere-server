d0d5a5e152b2fc375286aa170476fa818845b63e
binary_ho <dfghcvb11@naver.com>
2024-03-14 16:40:21 +0900

    test : 학생 출석 조회 API Controller Slice Test 구현 (#117)

--- src/test/java/gdsc/binaryho/imhere/presentation/AttendanceControllerTest.java
+++ src/test/java/gdsc/binaryho/imhere/presentation/AttendanceControllerTest.java
@@ -18,9 +18,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import gdsc.binaryho.imhere.core.attendance.application.LecturerAttendanceService;
 import gdsc.binaryho.imhere.core.attendance.application.StudentAttendanceService;
+import gdsc.binaryho.imhere.core.attendance.application.StudentRecentAttendanceResponse;
 import gdsc.binaryho.imhere.core.attendance.controller.AttendanceController;
 import gdsc.binaryho.imhere.core.attendance.model.request.AttendanceRequest;
 import gdsc.binaryho.imhere.core.attendance.model.response.LecturerAttendanceResponse;
+import gdsc.binaryho.imhere.core.attendance.model.response.StudentAttendanceResponse;
 import java.time.ZoneOffset;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -34,6 +36,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @WebMvcTest(AttendanceController.class)
 public class AttendanceControllerTest {
+
+    private static final String ROLE_PARAM = "role=";
+    private static final String LECTURER_ROLE_PARAM = ROLE_PARAM + "lecturer";
+    private static final String STUDENT_ROLE_PARAM = ROLE_PARAM + "student";
+    private static final String RECENT_PARAM = "status=recent";
 
     @Autowired
     private MockMvc mockMvc;
@@ -56,7 +63,7 @@ public class AttendanceControllerTest {
 
         doNothing().when(studentAttendanceService).takeAttendance(attendanceRequest, lectureId);
 
-        mockMvc.perform(post(String.format("/api/lecture/%d/attendance", lectureId))
+        mockMvc.perform(post(String.format("/api/lecture/%d/attendance?" + STUDENT_ROLE_PARAM, lectureId))
                 .with(csrf())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(attendanceRequest))
@@ -67,13 +74,68 @@ public class AttendanceControllerTest {
 
     @Test
     @WithMockUser
-    void 강의의_전체_출석_정보를_조회한다() throws Exception {
+    void 학생은_최근_출석_정보를_조회할_수_있다() throws Exception {
+        long lectureId = 7L;
+        long milliseconds = MOCK_ATTENDANCE.getTimestamp().toInstant(ZoneOffset.UTC).toEpochMilli();
+        StudentRecentAttendanceResponse response = new StudentRecentAttendanceResponse(
+            List.of(MOCK_ATTENDANCE.getTimestamp().toString()));
+        given(studentAttendanceService.getStudentRecentAttendance(lectureId))
+            .willReturn(response);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(String.format("/api/lecture/%d/attendance?" + STUDENT_ROLE_PARAM + "&" + RECENT_PARAM, lectureId, milliseconds))
+                    .with(csrf())
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+
+            .andExpect(jsonPath("$.timestamps").isNotEmpty())
+            .andExpect(result -> {
+                String timestamp = JsonPath.read(result.getResponse().getContentAsString(),
+                    "$.timestamps[0]");
+                assertThat(MOCK_ATTENDANCE.getTimestamp().toString()).contains(timestamp);
+            });
+    }
+
+    @Test
+    @WithMockUser
+    void 학생은_지정_날짜의_출석_정보를_조회할_수_있다() throws Exception {
+        long lectureId = 7L;
+        long milliseconds = MOCK_ATTENDANCE.getTimestamp().toInstant(ZoneOffset.UTC).toEpochMilli();
+        StudentAttendanceResponse response = new StudentAttendanceResponse(
+            List.of(MOCK_ATTENDANCE));
+        given(studentAttendanceService.getStudentDayAttendance(lectureId, milliseconds))
+            .willReturn(response);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(String.format("/api/lecture/%d/attendance?timestamp=%d&" + STUDENT_ROLE_PARAM, lectureId, milliseconds))
+                    .with(csrf())
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+
+            .andExpect(jsonPath("$.attendances").isNotEmpty())
+            .andExpect(
+                jsonPath("$.attendances[0].distance").value(MOCK_ATTENDANCE.getDistance()))
+            .andExpect(
+                jsonPath("$.attendances[0].accuracy").value(MOCK_ATTENDANCE.getAccuracy()))
+
+            .andExpect(result -> {
+                String timestamp = JsonPath.read(result.getResponse().getContentAsString(),
+                    "$.attendances[0].timestamp");
+                assertThat(MOCK_ATTENDANCE.getTimestamp().toString()).contains(timestamp);
+            });
+    }
+
+    @Test
+    @WithMockUser
+    void 강사는_자신의_강의의_전체_출석_정보를_조회할_수_있다() throws Exception {
         long lectureId = 7L;
         LecturerAttendanceResponse lecturerAttendanceResponse = new LecturerAttendanceResponse(
             List.of(MOCK_ATTENDANCE));
         given(lecturerAttendanceService.getLecturerAttendances(lectureId)).willReturn(lecturerAttendanceResponse);
 
-        mockMvc.perform(MockMvcRequestBuilders.get(String.format("/api/lecture/%d/attendance", lectureId))
+        mockMvc.perform(MockMvcRequestBuilders.get(String.format("/api/lecture/%d/attendance?" + LECTURER_ROLE_PARAM, lectureId))
                 .with(csrf())
             )
             .andDo(print())
@@ -97,7 +159,7 @@ public class AttendanceControllerTest {
 
     @Test
     @WithMockUser
-    void 특정_날짜의_출석_정보를_조회한다() throws Exception {
+    void 강사는_특정_날짜의_출석_정보를_조회할_수_있다() throws Exception {
         long lectureId = 7L;
         long milliseconds = MOCK_ATTENDANCE.getTimestamp().toInstant(ZoneOffset.UTC).toEpochMilli();
         LecturerAttendanceResponse lecturerAttendanceResponse = new LecturerAttendanceResponse(
@@ -106,7 +168,7 @@ public class AttendanceControllerTest {
             lecturerAttendanceResponse);
 
         mockMvc.perform(
-                MockMvcRequestBuilders.get(String.format("/api/lecture/%d/attendance?timestamp=%d", lectureId, milliseconds))
+                MockMvcRequestBuilders.get(String.format("/api/lecture/%d/attendance?timestamp=%d&" + LECTURER_ROLE_PARAM, lectureId, milliseconds))
                     .with(csrf())
             )
             .andDo(print())
